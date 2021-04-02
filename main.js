1a8cac64a1fb43e034969a38e73f13caaa925724
Dustin Mac <sonnyduluxwatershield@gmail.com>
2021-04-02 02:06:31 -0500

    fixing and validating all files

--- main.js
+++ main.js
@@ -8,8 +8,7 @@ $(document).ready(function () {
             array[j] = temp;
         }
         return array;
-    };
-
+    }
     $("button.generate").click(function () {
         var choice = document.getElementById("numbers").value;
         var difficulty1 = document.getElementById("trivia_difficulty").value;
@@ -17,10 +16,8 @@ $(document).ready(function () {
         var url =
             `https://opentdb.com/api.php?amount=${choice}&category=${category1}&difficulty=${difficulty1}&type=multiple`;
         $.get(url, function (data) {
-            console.log(data);
             var htmlString = "";
             answers = [];
-            
             for (var i in data.results) {
                 htmlString += `<div class="card card${i}" id="${i}">
                                     <h3 class="card-title">${data.results[i].question}</h3>
@@ -28,33 +25,34 @@ $(document).ready(function () {
                 data.results[i].incorrect_answers.push(data.results[i].correct_answer);
                 answers.push(data.results[i].correct_answer);
                 randomIT(data.results[i].incorrect_answers);
-                for(var j in data.results[i].incorrect_answers){
-                    htmlString+=`<label id="l${i}${j}"><input type="radio" name="question" id=${i}${j} value="${data.results[i].incorrect_answers[j]}">${data.results[i].incorrect_answers[j]}</label><br>`;
+                for (var j in data.results[i].incorrect_answers) {
+                    htmlString += `<label id="l${i}${j}"><input type="radio" name="question" id=${i}${j} value="${data.results[i].incorrect_answers[j]}">${data.results[i].incorrect_answers[j]}</label><br>`;
                 }
                 htmlString += `</form></div>`;
             }
             $("div.trivia").html(htmlString);
         });
     });
-    $("button.submit").click(function(){
-        CorrectAnswer=0;
-        
-        for(var i in answers){
-            for(var j = 0; j < 4; j++) {
+    $("button.submit").click(function () {
+        CorrectAnswer = 0;
+
+        for (var i in answers) {
+            for (var j = 0; j < 4; j++) {
                 x = `${i}${j}`;
                 y = `l${i}${j}`;
-                
-                if(document.getElementById(x).value == answers[i]){
-                    document.getElementById(y).style.color="blue";
-                    if(document.getElementById(x).checked== true){
-                        CorrectAnswer+=1;
+
+                if (document.getElementById(x).value == answers[i]) {
+                    document.getElementById(y).style.color = "white";
+                    document.getElementById(y).style.backgroundColor="blue";
+                    if (document.getElementById(x).checked == true) {
+                        CorrectAnswer += 1;
                     }
+                } else {
+                    document.getElementById(y).style.color = "white";
+                    document.getElementById(y).style.backgroundColor="red";
                 }
-                else{
-                    document.getElementById(y).style.color = "red";
-                }
-            };
-        };
-        alert(`You have scored ${CorrectAnswer}/${answers.length} point!!!`)
+            }
+        }
+        alert(`You have scored ${CorrectAnswer}/${answers.length} point!!!`);
     });
 });
